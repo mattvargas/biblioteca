1e05698d6d53091826997fb93ede78a6aff50092
Matt <matt@Macbook-Pro.local>
2018-04-06 00:16:49 -0300

    editando o visu 1

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {ProvedorProvider} from "../../providers/provedor/provedor";
-import Livro = livro.Livro;
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
