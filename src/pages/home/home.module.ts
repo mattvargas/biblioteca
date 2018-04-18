bd6a7913fe39b6b5ecb3c66bbfab05980db4a6d7
Matheus Vargas <matheusvargas@MacBook-Pro-de-Matheus.local>
2018-04-18 20:24:19 -0300

    tentando ajeitar a logo

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-
 import {HomePage} from "./home";
-import {LoginPage} from "../login/login";
 
 @NgModule({
   declarations: [
    HomePage,
-    LoginPage,
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
